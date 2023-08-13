0eca97045d3033063c757945d8e7f5851cae78e1
Nikita Milovanov <nizygg@gmail.com>
2023-08-13 18:53:58 +0300

    Flake8 editing

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -68,7 +68,7 @@ DATABASES = {
         'PASSWORD': os.getenv('POSTGRES_PASSWORD', ''),
         'HOST': os.getenv('DB_HOST', ''),
         'PORT': os.getenv('DB_PORT', 5432)
-        }
+    }
 }
 
 
